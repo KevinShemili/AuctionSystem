dad3b8347e92f6bec58dd6f2f1cafd3ec103d766
KevinShemili <kevinshemili5@gmail.com>
2025-05-02 15:19:57 +0200

    Configure Swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,23 +1,64 @@
+using Infrastructure.DependencyConfigurations;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+	.AddInfrastructureLayer(builder.Configuration);
 
+builder.Configuration
+	.AddJsonFile("appsettings.json")
+	.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(config => {
+	config.EnableAnnotations();
+
+	config.SwaggerDoc("v1", new OpenApiInfo {
+		Title = "Architecture",
+		Version = "v1"
+	});
+
+	config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+		In = ParameterLocation.Header,
+		Description = "JWT Authorization",
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		Scheme = "bearer",
+		BearerFormat = "JWT"
+	});
+
+	config.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new List<string>()
+		}
+	});
+});
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+ConfigureSwagger(app);
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+static void ConfigureSwagger(WebApplication app) {
+	app.UseDeveloperExceptionPage();
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
