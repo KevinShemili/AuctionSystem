4006b88f77fdbd0d69c238d9858f5095b504c9fe
KevinShemili <kevinshemili5@gmail.com>
2025-05-03 14:09:18 +0200

    Configure Dependency Injection for repostiories

--- Infrastructure/DependencyConfigurations/Dependencies.cs
+++ Infrastructure/DependencyConfigurations/Dependencies.cs
@@ -1,4 +1,8 @@
-﻿using Infrastructure.Persistence;
+﻿using Application.Contracts.Repositories;
+using Application.Contracts.Repositories.Common;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Infrastructure.Persistence.Repositories.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +14,7 @@ namespace Infrastructure.DependencyConfigurations {
 			IConfiguration configuration) {
 
 			services.ConfigureDatabaseConnection(configuration);
-
+			services.ConfigureScopedServices();
 
 			return services;
 		}
@@ -22,6 +26,17 @@ namespace Infrastructure.DependencyConfigurations {
 
 			services.AddDbContext<DatabaseContext>(options =>
 				options.UseNpgsql(connString, b => b.MigrationsAssembly("Infrastructure")));
+		}
+
+		private static void ConfigureScopedServices(this IServiceCollection services) {
+			services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+			services.AddScoped<IUserRepository, UserRepository>();
+			services.AddScoped<IAuthenticationTokenRepository, AuthenticationTokenRepository>();
+			services.AddScoped<IUserTokenRepository, UserTokenRepository>();
+			services.AddScoped<IPermissionRepository, PermissionRepository>();
+			services.AddScoped<IRolePermissionRepository, RolePermissionRepository>();
+			services.AddScoped<IRoleRepository, RoleRepository>();
+			services.AddScoped<IUserRoleRepository, UserRoleRepository>();
 		}
 	}
 }
